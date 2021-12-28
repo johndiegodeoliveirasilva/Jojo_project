7a3022343366e7409e01ebcbf6009772c530e3b9
John Diego <johnoliveirasilva9@gmail.com>
2021-12-28 00:03:08 -0200

    Add Seed to Person

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
 require 'net/http'
 # Estado
+Person.destroy_all
+Cidade.destroy_all
 Estado.destroy_all
 uf_cidades_url = URI('https://gist.githubusercontent.com/letanure/3012978/raw/2e43be5f86eef95b915c1c804ccc86dc9790a50a/estados-cidades.json')
 estados_cidades = JSON.parse(Net::HTTP.get(uf_cidades_url))
@@ -13,3 +15,12 @@ estados_cidades['estados'].each do |estado|
   end
 end
 p "Estados e Cidades carregados com sucesso!"
+
+1000.times do |t|
+  Person.create(name: Faker::Name.name, 
+                age: Faker::Number.within(range: 1..90),
+                email: Faker::Internet.email,
+                cidade_id: Random.rand(Cidade.first.id..Cidade.last.id))
+end
+
+p "Pessoas carregados com sucesso!"
