c6ccd34b59dc93fef2190606161dfa7ce999eb6a
John Diego <johnoliveirasilva9@gmail.com>
2022-01-05 22:17:00 -0200

    Configure Sidekiq

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
+require 'sidekiq/web'
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root "exports#index"
   resources :exports, only: [:index]
+  mount Sidekiq::Web => '/sidekiq'
 end
