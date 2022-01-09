f2b90f97ce432a0a1a6ed26839885bfccacd0e20
John Diego <johnoliveirasilva9@gmail.com>
2022-01-08 23:52:39 -0200

    Add Export

--- app/jobs/export_job.rb
+++ app/jobs/export_job.rb
@@ -1,8 +1,19 @@
+require 'spreadsheet'
 class ExportJob < ApplicationJob
   queue_as :exports
+  
+  def perform(*guest)
+    person = Spreadsheet::Workbook.new
+    sheet1 = person.create_worksheet
+    sheet1.row(0).concat Person.attribute_names.select{|t| !t.include?("_")}
+    Person.select("id, name, age, email").each_with_index do |per, index|
+      sheet1.row(index + 1).push(per.id, per.name, per.age, per.email)
+    end
+    create_folter
+    person.write "/tmp/exports/export-#{DateTime.current.strftime("%Y%m%d%S%s")}.xls"
+  end
 
-  def perform(*args)
-    puts 1 + 2
-    sleep 2
+  def create_folter
+    Dir.mkdir('/tmp/exports') unless Dir.exist?('/tmp/exports')
   end
 end
